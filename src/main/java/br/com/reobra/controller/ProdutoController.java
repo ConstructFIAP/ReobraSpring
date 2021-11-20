58b43f9cac1c2679c09843af2b5124f8636370b5
William Poletto <poletto123@gmail.com>
2021-11-20 14:46:16 -0300

    Adicionando endpoint para imagem de produtos

--- src/main/java/br/com/reobra/controller/ProdutoController.java
+++ src/main/java/br/com/reobra/controller/ProdutoController.java
@@ -10,6 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -34,6 +39,21 @@ public class ProdutoController {
     public ResponseEntity<Produto> getProduto(@PathVariable("id") long id){
         Produto produto = produtoRepository.getById(id);
         return new ResponseEntity<Produto>(produto, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/produto/imagem/{id}", method = RequestMethod.GET, produces = "image/jpg")
+    public @ResponseBody byte[] getImagem(@PathVariable("id") long id)  {
+        try {
+            String nomeArquivo = produtoRepository.getById(id).getCaminho_imagem();
+            InputStream is = this.getClass().getResourceAsStream("/" + nomeArquivo);
+            BufferedImage img = ImageIO.read(is);
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            ImageIO.write(img, "jpg", bao);
+            return bao.toByteArray();
+        } catch (IOException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
     }
 
     @PostMapping("/produto")
